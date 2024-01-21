936055430ca2d6e0ebd8f6cd7470fb207085de77
重国 <gzhongguo92@gmail.com>
2024-01-21 15:03:29 +0900

    Initial Commit

---
+++ practice_swift/practice_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  practice_swiftApp.swift
+//  practice_swift
+//
+//  Created by 重國光貴 on 2024/01/21.
+//
+
+import SwiftUI
+
+@main
+struct practice_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
